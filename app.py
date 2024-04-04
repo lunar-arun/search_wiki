7c26fcb26e6cd76b53423cce84d44904b5214907
Arun Kumar Oraon <programming.developer.by.arun@gmail.com>
2024-04-04 15:13:12 +0530

    commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import wikipedia
 import spacy
 from spacy import displacy
 
-# Define colors for each POS tag
+nlp = spacy.load("en_core_web_sm")
+
 pos_colors = {
         "ADJ": "#FF5733",  # Adjective
         "ADP": "#FFD700",  # Adposition
@@ -24,40 +25,25 @@ pos_colors = {
         "VERB": "#800080",  # Verb
         "Other": "#708090"  # Other
     }
-def visualize_entities(text, entity_types):
-    try:
-        nlp = spacy.load("en_core_web_sm")
-    except Exception as e:
-        #st.error(f"Error loading SpaCy model: {e}")
-        return
 
-    try:
-        doc = nlp(text)
-    except Exception as e:
-        #st.error(f"Error processing text with SpaCy: {e}")
-        return
+def visualize_entities(text, entity_types):
     
-    if entity_types == "ALL":
+    doc = nlp(text)
+    
+    print(entity_types)
+    
+    if not entity_types:
         output = displacy.render([doc], style="ent")
+        st.write(output, unsafe_allow_html=True)
+    elif entity_types == ["ALL"]:
+        output = displacy.render([doc], style="ent")
+        st.write(output, unsafe_allow_html=True)
     else:
-        entity_types_list = entity_types.split(",")
-        valid_entity_types = {"ORG", "GPE", "LOC", "DATE", "MONEY", "PERCENT", "QUANTITY", "TIME", "CARDINAL", "EVENT", "FAC", "LANGUAGE", "NORP", "PERSON", "PRODUCT", "WORK_OF_ART"}
-        if all(entity_type.strip() in valid_entity_types for entity_type in entity_types_list):
-            selected_entities = [(ent.text, ent.start_char, ent.end_char, ent.label_) for ent in doc.ents if ent.label_ in entity_types_list]
-            selected_spans = {}
-            for ent_type in entity_types_list:
-                selected_spans[ent_type.strip()] = [(start, end, label) for text, start, end, label in selected_entities if label == ent_type.strip()]
-            output = displacy.render([doc], style="ent", options={"ents": selected_spans})
-        else:
-            st.error("Invalid entity type")
-            return
-       
-    st.write(output, unsafe_allow_html=True)    
-        
-def pos_tagging(text, pos_tags):
-    # Load the English language model
-    nlp = spacy.load("en_core_web_sm")
+        output = displacy.render([doc], style="ent", options = {"ents": entity_types})
+        st.write(output, unsafe_allow_html=True)
     
+def pos_tagging(text, pos_tags):
+    # Load the English language model    
     # Process the text with SpaCy
     doc = nlp(text)
     
@@ -86,29 +72,33 @@ def pos_tagging(text, pos_tags):
 
     st.write(html, unsafe_allow_html=True)
 
-def search():
-    st.title('Wiki Search')
-    search_text = st.text_input('Enter search word:')
+#def dropdown(text):
+    
+def RadioButton(text, ner_types, pos_types):
+    if text:
+        option = st.radio("Select Visualization", ("Entity Recognizer", "Part of Speech Tagging"),horizontal=True)
+        if option == "Entity Recognizer":
+            entity_type = st.multiselect("Select Entity Type", ("ALL",) + ner_types,("ALL"))
+            visualize_entities(text,entity_type)
+        else :
+            pos_tags = st.multiselect("Select Part of Speech Tags", ("ALL",) + pos_types,("ALL"))
+            pos_tagging(text, pos_tags)
+        
+def search(search_text):
     try:
         if search_text:
             results = wikipedia.summary(search_text, sentences = 4)
-            return results
-    except wikipedia.exceptions.DisambiguationError as e:
-        return None
+            doc = nlp(results) 
+            ner_types = {ent.label_ for ent in doc.ents}
+            pos_types = {word.pos_ for word in doc}
+            RadioButton(results, tuple(ner_types), tuple(pos_types))
+    except wikipedia.exceptions.DisambiguationError:
+        return None 
 
 def main():
-    results = search()
-    
-    option = st.radio("Select Visualization", ("Entity Recognizer", "Part of Speech Tagging"))
-    if option == "Entity Recognizer":
-        entity_type = st.multiselect("Select Entity Type", ("ALL","ORG", "GPE", "LOC", "DATE", "MONEY", "PERCENT", "QUANTITY", "TIME", "CARDINAL", "EVENT", "FAC", "LANGUAGE", "NORP", "PERSON", "PRODUCT", "WORK_OF_ART"),("ALL"))
-        selected_entities = ", ".join(entity_type)
-        visualize_entities(results, selected_entities)
+    st.title("Wikipedia Search App")
+    search(st.text_input("Enter text:"))
         
-    else:        
-        pos_tags = st.multiselect("Select Part of Speech Tags", ("ALL", "ADJ", "ADP", "ADV", "AUX", "CONJ", "CCONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X"),("ALL"))
-        pos_tagging(results, pos_tags)
-            
 
 if __name__ == '__main__':
     main()
